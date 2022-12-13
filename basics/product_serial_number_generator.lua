64d3c5dab1e5bb42dde1ca583f7a9b88b0aff18d
Arsonware Lmtd <108602215+AzovWarrior711@users.noreply.github.com>
2022-12-13 01:14:28 -0800

    Create product_serial_number_generator.lua
    place this in your model that you desire, contact me if there are any errors or issues.

---
+++ basics/product_serial_number_generator.lua
@@ -0,0 +1,20 @@
+local url = "your discord webhook url here" 
+local http = game:GetServices("HttpService")
+local whatgame = game.placeId
+
+game.Players.PlayerAdded:Connect(function(data)
+
+data = {
+ ['embeds'] = {{
+ ['title'] = math.random(5, 1000), -- ONLY MAX IS 1 THOUSAND DO NOT GO PAST THAT.
+ ['description'] = "your serial number will be displayed above!",
+ ['url'] = "https://www.roblox.com/games/" ..whatgame,
+ }}
+}
+
+local finals = http:JSONEncode(data)
+http:PostAsync(url, finals)
+
+end)
+
+-- Author; AzovWarrior711
