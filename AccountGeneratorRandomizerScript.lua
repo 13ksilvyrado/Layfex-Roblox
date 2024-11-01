ef70b2132e401d536fd49d8e41bc69d6bf834a0d
mars. <108602215+13ksilvyrado@users.noreply.github.com>
2024-11-01 10:41:24 -0700

    Update AccountGeneratorRandomizerScript.lua

--- AccountGeneratorRandomizerScript.lua
+++ AccountGeneratorRandomizerScript.lua
@@ -11,11 +11,3 @@ local TextChatCommand = TextChatService:WaitForChild("TextChatCommand")
 local TextChatCommands = TextChatService:WaitForChild("TextChatCommands")
 local TextChatCommand1 = TextChatCommands:WaitForChild("Command1")
 local TextChat 
-
--- make it where when a random person joins it generates a 5 digit number in the output
--- make it where only the approved id can use the command
--- make it where the command is /random
--- make it where the command will output the random number in the output
--- make it where the command will output the random number in the chat
--- make it where the command will output the random number in the chat as the player
--- make it where the command will output the random number in the chat as the player with the id and telling the player its their new ID
